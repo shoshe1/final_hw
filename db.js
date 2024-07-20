45c7db9e0d0a5a418f71e3d19f97f90becd0ee75
shoshe1 <shahdbaloum2006@gmail.com>
2024-07-20 22:29:28 +0300

    i added a deal with an error if it occurs

--- db.js
+++ db.js
@@ -8,6 +8,12 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
 
 module.exports = connection;
